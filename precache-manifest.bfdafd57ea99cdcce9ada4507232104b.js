5fa807e53dcf55d294c2f3fb7a4c096ef0f7dde4
Aditya Ranjan Singh <adityaranjansingh@adityas-mbp.home>
2019-08-02 00:25:51 +0100

    trying to replace build folder directly

---
+++ precache-manifest.bfdafd57ea99cdcce9ada4507232104b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054fcea843c802994c8e5fc8e09168d4",
+    "url": "/TextTranslate/index.html"
+  },
+  {
+    "revision": "ac5ad331ed61b5e8d958",
+    "url": "/TextTranslate/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7b41228cc5d9d6b5f2eb",
+    "url": "/TextTranslate/static/css/main.9f0f7a7b.chunk.css"
+  },
+  {
+    "revision": "ac5ad331ed61b5e8d958",
+    "url": "/TextTranslate/static/js/2.7955a62c.chunk.js"
+  },
+  {
+    "revision": "7b41228cc5d9d6b5f2eb",
+    "url": "/TextTranslate/static/js/main.82f7520b.chunk.js"
+  },
+  {
+    "revision": "dbbac4ed7c86ec601e1f",
+    "url": "/TextTranslate/static/js/runtime~main.932fe1d1.js"
+  }
+]);
