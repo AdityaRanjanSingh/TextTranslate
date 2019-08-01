f35e3fff3dd2d9631463af39a889f2ebc4bd22ba
Aditya Ranjan Singh <adityaranjansingh@adityas-mbp.home>
2019-08-02 00:34:17 +0100

    replacing files with build folder

---
+++ precache-manifest.b1c6cb1db427305e400e97758a50c375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5425a2a0478d4ee399d09100809c8991",
+    "url": "/AdityaRanjanSingh/TextTranslate/index.html"
+  },
+  {
+    "revision": "ac5ad331ed61b5e8d958",
+    "url": "/AdityaRanjanSingh/TextTranslate/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0a8f0c850f2980b4753e",
+    "url": "/AdityaRanjanSingh/TextTranslate/static/css/main.9f0f7a7b.chunk.css"
+  },
+  {
+    "revision": "ac5ad331ed61b5e8d958",
+    "url": "/AdityaRanjanSingh/TextTranslate/static/js/2.7955a62c.chunk.js"
+  },
+  {
+    "revision": "0a8f0c850f2980b4753e",
+    "url": "/AdityaRanjanSingh/TextTranslate/static/js/main.30e7f497.chunk.js"
+  },
+  {
+    "revision": "0a9399b8b8745318d0db",
+    "url": "/AdityaRanjanSingh/TextTranslate/static/js/runtime~main.3896b8d1.js"
+  }
+]);
--- src/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const TRANSLATE_TEXT = "TRANSLATE_TEXT";
-export const INPUT_ACTION = "INPUT_ACTION";
-export const SELECT_ACTION = "SELECT_ACTION";
